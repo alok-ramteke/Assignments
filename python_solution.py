35c3bba89de788b33b1156fd0296f6e9d7dc498b
alok-ramteke <alokramteke28@gmail.com>
2019-03-20 19:33:22 +0530

    #solution- python multiprocessing

--- python_solution.py
+++ python_solution.py
@@ -1,3 +1,6 @@
+import multiprocessing 
+
+
 class Solution:
     """
     Group anagrams together from a given array of strings
@@ -22,3 +25,35 @@ class Solution:
             else:
                 anagram_dict[word] = [i]
         return [value for value in anagram_dict.values()]
+
+
+def square(numbers, result, square_sum): 
+    """ 
+    function to square a given list 
+    """
+    for idx, num in enumerate(numbers): 
+        result[idx] = num * num 
+
+    square_sum.value = sum(result) 
+
+    print("Result(process p1): {}".format(result[:])) 
+
+    print("Sum of squares(process p1): {}".format(square_sum.value)) 
+
+if __name__ == "__main__": 
+
+    numbers = [1,2,3,4] 
+
+    result = multiprocessing.Array('i', 4) 
+ 
+    square_sum = multiprocessing.Value('i') 
+
+    p1 = multiprocessing.Process(target=square, args=(numbers, result, square_sum)) 
+
+    p1.start() 
+
+    p1.join() 
+
+    print("Result(main program): {}".format(result[:])) 
+
+    print("Sum of squares(main program): {}".format(square_sum.value))
